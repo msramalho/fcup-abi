b070543c7aedfc6f533b330b0b86cee4d69490d6
msramalho <up201403027@fe.up.pt>
2019-04-13 17:09:14 +0100

    GAP in protein instead of literal

--- bioseq/proteinseq.py
+++ bioseq/proteinseq.py
@@ -3,7 +3,7 @@ from bioseq import BioSeq
 
 class ProteinSeq(BioSeq):
     """Biological Sequence specific for PROTEINS"""
-    valid_tokens = {'P', 'F', '_', 'M', 'I', 'R', 'K', 'A', 'L', 'V', 'Q', 'E', 'C', 'N', 'W', 'H', 'S', 'T', 'D', 'G', 'Y'}
+    valid_tokens = {'P', 'F', GAP, 'M', 'I', 'R', 'K', 'A', 'L', 'V', 'Q', 'E', 'C', 'N', 'W', 'H', 'S', 'T', 'D', 'G', 'Y'}
 
     def __init__(self, sequence):
         super().__init__(sequence, "PROTEIN")
