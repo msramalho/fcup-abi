dda94af8a51c8964ac5bad3b7394e92209363c8f
msramalho <up201403027@fe.up.pt>
2019-06-16 00:03:43 +0100

    included name in protein seq

--- bioseq/proteinseq.py
+++ bioseq/proteinseq.py
@@ -6,9 +6,10 @@ class ProteinSeq(BioSeq):
     """Biological Sequence specific for PROTEINS"""
     valid_tokens = {'P', 'F', GAP, 'M', 'I', 'R', 'K', 'A', 'L', 'V', 'Q', 'E', 'C', 'N', 'W', 'H', 'S', 'T', 'D', 'G', 'Y'}
 
-    def __init__(self, sequence):
+    def __init__(self, sequence, name=None):
         super().__init__(sequence, "PROTEIN")
         self._assert_valid_sequence()
+        if name: self.name = name
 
     def gc_content(self):
         raise NotImplementedError("The class ProteinSeq does not have a meaningful implementation for gc_content")
