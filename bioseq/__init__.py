01342479afa32a618fb77e4a3f922cf763645ced
msramalho <up201403027@fe.up.pt>
2019-06-16 00:03:21 +0100

    including blast files

--- bioseq/__init__.py
+++ bioseq/__init__.py
@@ -1,6 +1,7 @@
 from bioseq.bioseq import BioSeq
 from bioseq.dnaseq import DNASeq
 from bioseq.rnaseq import RNASeq
+from bioseq.blast import Blast
 from bioseq.proteinseq import ProteinSeq
 from bioseq.utils import *
 from bioseq.matrix import *
