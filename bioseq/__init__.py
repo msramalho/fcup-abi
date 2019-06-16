96ef99a0a94637fffd44ddce6ce41016ed3ed2bf
msramalho <up201403027@fe.up.pt>
2019-06-16 11:37:49 +0100

    include MSA in init

--- bioseq/__init__.py
+++ bioseq/__init__.py
@@ -2,6 +2,7 @@ from bioseq.bioseq import BioSeq
 from bioseq.dnaseq import DNASeq
 from bioseq.rnaseq import RNASeq
 from bioseq.blast import Blast
+from bioseq.msa import MSA
 from bioseq.proteinseq import ProteinSeq
 from bioseq.utils import *
 from bioseq.matrix import *
