d322a347052c5127aae7b52a9cfcb83879cfb752
msramalho <up201403027@fe.up.pt>
2019-06-16 11:38:07 +0100

    preliminary tests for MSA

---
+++ test/test_msa.py
@@ -0,0 +1,31 @@
+import unittest
+from bioseq import Blast, ProteinSeq, DNASeq, MSA
+from bioseq.utils import *
+
+db = "bioseq/resources/seqdump.txt"
+fq = "bioseq/resources/source.fasta"
+sm = read_substitution_matrix_file("test/blosum62.mat")
+sm2 = substitution_matrix("ATGC", 1, -1)
+
+
+class TestMSA(unittest.TestCase):
+    def test_constructor(self):
+        m = MSA(sm, -3)
+        self.assertEqual(m.sm, sm)
+        self.assertEqual(m.g, -3)
+
+    def test_align(self):
+        seqs = [DNASeq("ATAGC"), DNASeq("AACC")]
+        m = MSA(sm2, -1)
+        self.assertEqual("ATACC", str(m.align(seqs)))
+
+    def test_consensus(self):
+        m = MSA(sm2, -1)
+        c = m.consensus(DNASeq("ATAGC"), DNASeq("A_ACC"))
+        self.assertEqual(type(c), bioseq.dnaseq.DNASeq)
+        self.assertEqual(str(c), "ATACC")
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
