610ce620b59ce378f27813a9c50e4de0a0036466
msramalho <up201403027@fe.up.pt>
2019-06-16 18:24:53 +0100

    ptree updated to work with aligned and not aligned

--- bioseq/ptree.py
+++ bioseq/ptree.py
@@ -1,7 +1,7 @@
 from bioseq import BioSeq
 from .matrix import Matrix
 from .utils import GAP
-
+from tqdm import tqdm
 
 class PTree:
     """Implements Phylogenetic Trees construction"""
@@ -10,13 +10,30 @@ class PTree:
         """Initialize the PTree class with the substitution matrix and gap penalty"""
         self.sm = sm
         self.g = g
+        self.ignore=0
 
     def distance_matrix(self, seqs):
+        """Returns the distance matrix between all pairs in seqs, assumes not aligned"""
+        m = Matrix(len(seqs), value=self.ignore)
+        with tqdm(total=sum(range(len(seqs)))) as pbar:
+            for i in range(len(seqs)):
+                for j in range(i + 1, len(seqs)):
+                    score, traceback = seqs[i].global_align_multiple_solutions(seqs[j], self.sm, self.g)
+                    l, r = next(seqs[i].recover_global_align_multiple_solutions(seqs[j], traceback))
+                    m[i][j] = l.hamming_distance(r)
+                    pbar.update()
+        return m
+
+    def distance_matrix_aligned(self, seqs):
         """Returns the distance matrix between all pairs in seqs"""
-        m = Matrix(len(seqs))
-        for i in range(len(seqs)):
-            for j in range(i + 1, len(seqs)):
-                score, traceback = seqs[i].global_align_multiple_solutions(seqs[j], self.sm, self.g)
-                l, r = next(seqs[i].recover_global_align_multiple_solutions(seqs[j], traceback))
-                m[i][j] = sum(c == GAP for c in l) + sum(c == GAP for c in r)
-        return m.symmetric()
+        m = Matrix(len(seqs), value=self.ignore)
+        with tqdm(total=sum(range(len(seqs)))) as pbar:
+            for i in range(len(seqs)):
+                for j in range(i + 1, len(seqs)):
+                    m[i][j] = seqs[i].hamming_distance(seqs[j])
+                    pbar.update()
+        return m
+
+
+    def clustering(self, m):
+        mn = m.min(self.ignore)
