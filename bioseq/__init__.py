18e549e6c77a0d7fb268f3381fdc6b6a32a3f1e7
msramalho <up201403027@fe.up.pt>
2019-06-16 17:22:52 +0100

    included ptree in init

--- bioseq/__init__.py
+++ bioseq/__init__.py
@@ -3,6 +3,7 @@ from bioseq.dnaseq import DNASeq
 from bioseq.rnaseq import RNASeq
 from bioseq.blast import Blast
 from bioseq.msa import MSA
+from bioseq.ptree import PTree
 from bioseq.proteinseq import ProteinSeq
 from bioseq.utils import *
 from bioseq.matrix import *
