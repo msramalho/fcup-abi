fde55633738b8468259c30713ad62b31886e35e9
msramalho <up201403027@fe.up.pt>
2019-06-17 16:34:31 +0100

    100% coverage for blast

--- test/test_blast.py
+++ test/test_blast.py
@@ -5,7 +5,8 @@ from bioseq.utils import *
 
 db = "bioseq/resources/seqdump.txt"
 fq = "bioseq/resources/source.fasta"
-
+blosum = read_substitution_matrix_file("bioseq/resources/blosum62.mat")
+sm = substitution_matrix("ATGC_", 1, -1)
 
 class TestBlast(unittest.TestCase):
     def test_constructor(self):
@@ -26,9 +27,25 @@ class TestBlast(unittest.TestCase):
         self.assertEqual(b.forward_matching(ProteinSeq("PFMI"), ProteinSeq("PFMI"), (0, 0), 0.5), (1, 1))
         self.assertEqual(b.forward_matching(ProteinSeq("PFMI"), ProteinSeq("PFMI"), (3, 3), 0.5), (0, 0))
         self.assertEqual(b.forward_matching(ProteinSeq("PFMIPPM"), ProteinSeq("PFMIPPF"), (0, 0), 0.5), (3, 4))
+        self.assertEqual(b.forward_matching(ProteinSeq("PFMIPPM"), ProteinSeq("IIIIII"), (0, 0), 0.5), (1, 3))
         self.assertEqual(b.extend_hit(ProteinSeq("PFMI"), ProteinSeq("PFMI"), (0, 0), 0.5), 1.0)
         self.assertEqual(b.extend_hit(ProteinSeq("PFMI"), ProteinSeq("PFMI"), (3, 3), 0.5), 1.0)
         self.assertEqual(b.extend_hit(ProteinSeq("PFMIPPM"), ProteinSeq("PFMIPPF"), (0, 0), 0.5), 0.75)
+
+    def test_search(self):
+        b = Blast(db, 3)
+        q = next(read_fasta(fq))
+        similar = b.search(q, top=5)
+        self.assertEqual(len(similar), 5)
+        self.assertEqual(similar[0][0].name, "XP_016791527.1 cytochrome P450 2B6 [Pan troglodytes]")
+        self.assertAlmostEqual(similar[0][1], 0.9901960784313726, places=5)
+        self.assertAlmostEqual(similar[1][1], 0.9901960784313726, places=5)
+        self.assertAlmostEqual(similar[2][1], 0.9852941176470589, places=5)
+
+    #     m = MSA(blosum, -8)
+    #     seqs = [ProteinSeq("PHWASW"), ProteinSeq("HPHWA")]
+    #     self.assertEqual(str(m.align(seqs)), "(HPHWASW, [_PHWASW, HPHWA__])")
+
 
 
 if __name__ == '__main__':
